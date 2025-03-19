10f3e4596ae69932ba0cfc1fb0f6011b5aa7db8b
Sarwar Alam <100376398+srwr-tech@users.noreply.github.com>
2025-03-20 01:25:21 +0530

    Update EditTaskModal.jsx

--- src/component/EditTaskModal.jsx
+++ src/component/EditTaskModal.jsx
@@ -37,7 +37,7 @@ const EditTaskModal = ({ isOpen, task, onClose, onSave }) => {
     e.preventDefault();
     
     try {
-      const response = await fetch(`http://localhost:5000/api/tasks/${task._id}`, {
+      const response = await fetch(`https://phenomenal-jalebi-e6bc93.netlify.app/api/tasks/${task._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
